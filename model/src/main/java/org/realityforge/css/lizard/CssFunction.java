d7751d87d56bf0d5d090e33a4cd033b641a680a1
Peter Donald <peter@realityforge.org>
2014-03-23 18:22:30 +1100

    Avoid excessive static imports

--- model/src/main/java/org/realityforge/css/lizard/CssFunction.java
+++ model/src/main/java/org/realityforge/css/lizard/CssFunction.java
@@ -1,18 +1,16 @@
 package org.realityforge.css.lizard;
 
 import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
  * Annotation used to identify a function to be exported to the runtime.
  */
-@Target( METHOD )
-@Retention( RUNTIME )
+@Target( ElementType.METHOD )
+@Retention( RetentionPolicy.RUNTIME )
 @Documented
 public @interface CssFunction
 {
